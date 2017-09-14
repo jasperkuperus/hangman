0866312ff2b78a2316efc86fd19edd055492d03f
Jasper Kuperus <mail@jasperkuperus.nl>
2017-09-14 20:35:22 +0200

    Initial Hangman with only letters

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Hangman from '../Hangman';
 
-const App = () => {
-  return (
-    <div>App</div>
-  );
-};
+const words = ['Jorrit', 'Afgestudeerd', 'Held'];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wordIndex: 0,
+      success: false,
+    };
+
+    this.handleSuccess = this.handleSuccess.bind(this);
+  }
+
+  handleSuccess() {
+    this.setState({ success: true });
+
+    setTimeout(() => {
+      const newIndex = (this.state.wordIndex + 1) % words.length;
+      this.setState({ wordIndex: newIndex, success: false });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="hangman-container">
+          <Hangman
+            word={words[this.state.wordIndex]}
+            onSuccess={this.handleSuccess}
+          />
+          {this.state.success &&
+            <div>YAY!</div>
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Hangman/index.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import _ from 'lodash';
+
+class Hangman extends Component {
+  static propTypes = {
+    word: PropTypes.string.isRequired,
+    onSuccess: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = this.createInitialState(props.word);
+
+    this.keyListener = this.keyListener.bind(this);
+    this.renderLetter = this.renderLetter.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyListener);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.keyListener);
+  }
+
+  componentDidUpdate() {
+    if (this.state.word !== this.props.word) {
+      this.setState(this.createInitialState(this.props.word));
+    }
+  }
+
+  createInitialState(word) {
+    return {
+      word,
+      letters: word.split('').map(letter => letter.toLowerCase()),
+      guesses: [],
+    };
+  }
+
+  keyListener(event) {
+    const { letters } = this.state;
+
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+      const newGuesses = this.state.guesses.concat([event.key.toLowerCase()]);
+      this.setState({ guesses: newGuesses });
+
+      if (_.difference(letters, newGuesses).length === 0) {
+        this.props.onSuccess();
+      }
+    }
+  }
+
+  renderLetter(letter, index) {
+    const { guesses } = this.state;
+    const letterAvailable = guesses.includes(letter);
+
+    return (
+      <div key={index} className={classNames('letter', { letter_available: letterAvailable })}>
+        { letterAvailable ? letter : '_'}
+      </div>
+    );
+  }
+
+  render() {
+    const word = this.props.word.toLowerCase();
+
+    return (
+      <div className="hangman">
+        {word.split('').map(this.renderLetter)}
+      </div>
+    );
+  }
+}
+
+export default Hangman;
