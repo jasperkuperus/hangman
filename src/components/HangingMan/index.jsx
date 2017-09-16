9e6a6a54c7970d3ac3f4f89d7fd1f0d88314ea86
Jasper Kuperus <mail@jasperkuperus.nl>
2017-09-16 11:53:32 +0200

    New styling and a stut

--- src/components/HangingMan/index.jsx
+++ src/components/HangingMan/index.jsx
@@ -4,7 +4,8 @@ import classNames from 'classnames';
 
 const HangingMan = props => (
   <div className="hanging-man">
-    <div className={classNames('hanging-man-part', 'hanging-man-rope', { invisible: props.errors < 4 })} />
+    <div className={classNames('hanging-man-part', 'hanging-man-rope', { invisible: props.errors < 5 })} />
+    <div className={classNames('hanging-man-part', 'hanging-man-stut', { invisible: props.errors < 4 })} />
     <div className={classNames('hanging-man-part', 'hanging-man-top', { invisible: props.errors < 3 })} />
     <div className={classNames('hanging-man-part', 'hanging-man-pole', { invisible: props.errors < 2 })} />
     <div className={classNames('hanging-man-part', 'hanging-man-base', { invisible: props.errors < 1 })} />
