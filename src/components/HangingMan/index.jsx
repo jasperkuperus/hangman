d8c2a509358635a8f01d226ec770b91487967a8f
Jasper Kuperus <mail@jasperkuperus.nl>
2017-09-16 12:32:42 +0200

    Hanging man. For real

--- src/components/HangingMan/index.jsx
+++ src/components/HangingMan/index.jsx
@@ -4,11 +4,21 @@ import classNames from 'classnames';
 
 const HangingMan = props => (
   <div className="hanging-man">
-    <div className={classNames('hanging-man-part', 'hanging-man-rope', { invisible: props.errors < 5 })} />
-    <div className={classNames('hanging-man-part', 'hanging-man-stut', { invisible: props.errors < 4 })} />
-    <div className={classNames('hanging-man-part', 'hanging-man-top', { invisible: props.errors < 3 })} />
-    <div className={classNames('hanging-man-part', 'hanging-man-pole', { invisible: props.errors < 2 })} />
-    <div className={classNames('hanging-man-part', 'hanging-man-base', { invisible: props.errors < 1 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-leg-left', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-leg-right', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-spine', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-hand-left', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-hand-right', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'body-part', 'hanging-man-neck', { invisible: props.errors < 6 })} />
+    <div className={classNames('man-part', 'hanging-man-face', { invisible: props.errors < 6 })}>
+      <img src="/assets/images/face.png" alt="Hangman face" />
+    </div>
+
+    <div className={classNames('pole-part', 'hanging-man-rope', { invisible: props.errors < 5 })} />
+    <div className={classNames('pole-part', 'hanging-man-stut', { invisible: props.errors < 4 })} />
+    <div className={classNames('pole-part', 'hanging-man-top', { invisible: props.errors < 3 })} />
+    <div className={classNames('pole-part', 'hanging-man-pole', { invisible: props.errors < 2 })} />
+    <div className={classNames('pole-part', 'hanging-man-base', { invisible: props.errors < 1 })} />
   </div>
 );
 
